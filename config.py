17f162a15022e79a9bf40e5b9c8c4b5ecf4b5cd1
DaShaun <dashaun@dashaun.com>
2022-01-02 16:48:38 -0600

    default config and path

--- config.py
+++ config.py
@@ -1,12 +1,13 @@
 config = {}
-config["am"] = 400
+config["am"] = 700
 config["pm"] = 2000
 
 config["flip_horizontal"] = False
 config["flip_vertical"] = False
 config["metering_mode"] = "matrix"
+config["exposure"] = "auto"
 
-config["base_path"] = "/var/image"
+config["base_path"] = "/home/pi/image"
 config["height"] = 1536
 config["width"] = 2048
 config["quality"] = 35
